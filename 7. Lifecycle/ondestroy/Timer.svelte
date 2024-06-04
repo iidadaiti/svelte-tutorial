2060348945ec3094d6541b969a219f547832150d
Daichi Iida <daichi.iida@nextbeat.net>
2024-06-04 15:35:48 +0900

    ondestroy

---
+++ 7. Lifecycle/ondestroy/Timer.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { onInterval } from './utils.js';
+	import { onDestroy } from 'svelte';
+
+	let counter = 0;
+	let interval = 1000;
+	export let callback;
+
+	onInterval(callback, interval);
+</script>
+
+<p>
+	This component executes a callback every
+	{interval} millisecond{interval === 1 ? '' : 's'}
+</p>
+
+<style>
+	p {
+		border: 1px solid blue;
+		padding: 5px;
+	}
+</style>
