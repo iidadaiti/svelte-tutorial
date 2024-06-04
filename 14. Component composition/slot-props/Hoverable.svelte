f4dbbfa0fc3491fc48d363a2851167461ace0ae4
Daichi Iida <daichi.iida@nextbeat.net>
2024-06-04 17:58:18 +0900

    slot props

---
+++ 14. Component composition/slot-props/Hoverable.svelte
@@ -0,0 +1,15 @@
+<script>
+	let hovering;
+
+	function enter() {
+		hovering = true;
+	}
+
+	function leave() {
+		hovering = false;
+	}
+</script>
+
+<div role="button" tabindex="-1" on:mouseenter={enter} on:mouseleave={leave}>
+	<slot hovering={hovering} />
+</div>
