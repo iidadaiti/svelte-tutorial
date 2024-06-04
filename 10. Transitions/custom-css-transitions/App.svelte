36dc8ebdefa9713ba6e93d68d3eef2d715245734
Daichi Iida <daichi.iida@nextbeat.net>
2024-06-04 16:30:04 +0900

    custom css transitions

---
+++ 10. Transitions/custom-css-transitions/App.svelte
@@ -0,0 +1,59 @@
+<script>
+	import { elasticOut } from 'svelte/easing';
+
+	let visible = true;
+
+    // Is force!
+	function spin(node, { duration }) {
+		return {
+			duration,
+			css: (t) => {
+				const eased = elasticOut(t);
+
+				return `
+					transform: scale(${eased}) rotate(${eased * 1080}deg);
+					color: hsl(
+						${Math.trunc(t * 360)},
+						${Math.min(100, 1000 - 1000 * t)}%,
+						${Math.min(50, 500 - 500 * t)}%
+					);`;
+			}
+		};
+	}
+
+    function fade(node, { delay = 0, duration = 400 }) {
+        const o = +getComputedStyle(node).opacity;
+
+        return {
+            delay,
+            duration,
+            css: (t) => `opacity: ${t * o}`
+        };
+    }
+</script>
+
+<label>
+    <input type="checkbox" bind:checked={visible} />
+    visible
+</label>
+
+{#if visible}
+    <div class="centered" in:spin={{ duration: 8000 }} out:fade>
+        <span>transitions!</span>
+    </div>
+{/if}
+
+<style>
+    .centered {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    span {
+        position: absolute;
+        transform: translate(-50%, -50%);
+        font-size: 4em;
+    }
+</style>
