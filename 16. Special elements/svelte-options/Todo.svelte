98cbc3e42d696abe8994d2b243410f45eacf1686
Daichi Iida <daichi.iida@nextbeat.net>
2024-06-04 18:20:09 +0900

    svelte options

---
+++ 16. Special elements/svelte-options/Todo.svelte
@@ -0,0 +1,28 @@
+<svelte:options immutable/>
+
+<script>
+	import { afterUpdate } from 'svelte';
+	import flash from './flash.js';
+
+	export let todo;
+
+	let button;
+
+	afterUpdate(() => {
+		flash(button);
+	});
+</script>
+
+<button bind:this={button} type="button" on:click>
+	{todo.done ? '👍' : ''}
+	{todo.text}
+</button>
+
+<style>
+	button {
+		all: unset;
+		display: block;
+		cursor: pointer;
+		line-height: 1.5;
+	}
+</style>
