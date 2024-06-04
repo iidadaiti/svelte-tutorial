475bcaf34ad5d1c8b848a4cec457a17c9c7e6e7a
Daichi Iida <daichi.iida@nextbeat.net>
2024-06-04 14:51:00 +0900

    component this

---
+++ 6. Bindings/component-this/App.svelte
@@ -0,0 +1,8 @@
+<script>
+    import InputField from './InputField.svelte';
+
+	let field;
+</script>
+
+<button on:click={() => field.focus()}> Focus field </button>
+<InputField bind:this={field} />
